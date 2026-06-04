b0d245c6f4f7c6f41348d1246ea267271c6ff9cf
Jason Everett <Jason.Everett@unsw.edu.au>
2026-06-04 15:20:22 +1000

    Remove unneeded handler

--- inst/app/www/handlers.js
+++ inst/app/www/handlers.js
@@ -1,18 +1,5 @@
 $( document ).ready(function() {
   Shiny.addCustomMessageHandler('fun', function(arg) {
  
-  });
-
-  // selectize.js sets an inline width style on .selectize-input at initialisation.
-  // When the widget is inside a splitLayout cell (display: table-cell), the measured
-  // width can be the full sidebar width rather than the cell width, causing the input
-  // box to overflow its column. Removing the inline width lets the CSS rule
-  // (.shiny-input-container .selectize-input { width: 100% }) take over correctly.
-  function fixSelectizeWidths() {
-    $('.shiny-split-layout .selectize-input').css('width', '');
-    $('.shiny-split-layout .selectize-control').css('width', '');
-  }
-
-  // Run after a short delay to let selectize finish initialising
-  setTimeout(fixSelectizeWidths, 500);
+  })
 });
