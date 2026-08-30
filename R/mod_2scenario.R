5ac53565be4152955cf2d0a225390c05c705bdda
Jason Everett <Jason.Everett@unsw.edu.au>
2026-08-30 14:50:26 +1000

    Update mod_2scenario.R

--- R/mod_2scenario.R
+++ R/mod_2scenario.R
@@ -996,7 +996,7 @@ mod_2scenario_server <- function(id, cfg, tab_visible) {
     # no layout issues.
     output$leaflet_map <- leaflet::renderLeaflet({
       leaflet::leaflet() %>%
-        leaflet::addProviderTiles(leaflet::providers$CartoDB.Positron)
+        leaflet::addProviderTiles(leaflet::providers$OpenTopoMap)
     })
 
     # Observer 1: prepare WGS84 solution data whenever a new analysis completes.
